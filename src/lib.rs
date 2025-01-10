7b52529af92cdc960821d5ec380c9d2730c7bb07
lewisakura <lewi@lewisakura.moe>
2025-01-10 19:44:04 +0000

    ci: fix secret

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ use std::path::PathBuf;
 use anyhow::Context;
 use directories::UserDirs;
 
-#[inline(always)]
+#[must_use]
 pub fn version() -> &'static str {
 	env!("CARGO_PKG_VERSION")
 }
 
-#[inline(always)]
+#[must_use]
 pub fn directory() -> PathBuf {
 	// TODO: is this the best place for this?
 	UserDirs::new()
